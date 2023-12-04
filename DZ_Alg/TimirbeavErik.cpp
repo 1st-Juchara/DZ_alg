2cac79e44e70cacab882b5b5b309758b88f4f311
eriktimirbaev <timirbaeverik@yandex.ru>
2023-12-05 00:51:51 +0300

    Update TimirbeavErik.cpp

--- DZ_Alg/TimirbeavErik.cpp
+++ DZ_Alg/TimirbeavErik.cpp
@@ -5,31 +5,37 @@ using namespace std;
 
 vector<int> get_path(vector<vector<int>>& from, int start, int finish) {
     vector<int> path;
-    for (int v = finish; v != -1; v = from[start][v])
-        path.push_back(v);
+    for (int i = finish; i != -1; i = from[start][i])
+        path.push_back(i);
     reverse(path.begin(), path.end());
     return path;
 }
 
+
 void TimirbaevErik(ifstream& fin) {
 
-    int vertex_count = tryChoose(fin, 0, 9999);  int edge_count = tryChoose(fin, 0, vertex_count*vertex_count);
+    auto start = chrono::system_clock::now();
+
+    int vertex_count = tryChoose(fin, 0, 9999);  int edge_count = tryChoose(fin, 0, 9999);
     const int INF = 1e9;
 
     vector<vector<int>> matrix(vertex_count, vector<int>(vertex_count, INF));
 
     vector<vector<int>> from(vertex_count, vector<int>(vertex_count, -1));
 
-    for (int v = 0; v < vertex_count; v++) {
-        matrix[v][v] = 0;
+    for (int i = 1; i < vertex_count + 1; i++) {
+        matrix[i - 1][i - 1] = 0;
     }
 
     for (int i = 0; i < edge_count; i++) {
-        int a = tryChoose(fin), int b = tryChoose(fin), int weight = tryChoose(fin);
+        int a = tryChoose(fin, 0, vertex_count - 1);
+        int b = tryChoose(fin, 0, vertex_count - 1);
+        int weight = tryChoose(fin, -9999, 9999);
 
         if (matrix[a][b] > weight) {
             matrix[a][b] = weight;
             from[a][b] = a;
+
         }
     }
 
@@ -46,22 +52,28 @@ void TimirbaevErik(ifstream& fin) {
         }
     }
 
-    /*ofstream output_file;
-    output_file.open("output.txt", ios::out);
-    if (output_file.is_open()) {
-        for (int a = 0; a < vertex_count; a++) {
-            for (int b = 0; b < vertex_count; b++) {
-                output_file << matrix[a][b] << "\t";
-            }
-            output_file << "\n";
+    auto finish = chrono::system_clock::now();
+    auto time = chrono::duration_cast<chrono::microseconds>(finish - start).count();
+    cout << "Time for method: " << time << " microseconds" << endl << endl;
+
+    for (int i = 0; i < vertex_count; i++) {
+        for (int j = 0; j < vertex_count; j++) {
+            if (matrix[i][j] == INF) cout << "INF\t";
+            else cout << matrix[i][j] << "\t";
         }
-
-        output_file << endl << "The shortest path from 0 to 2" << endl << endl;
-
-        for (int v : get_path(from, 0, 2))
-            output_file << v << "\t";
-
-        output_file.close();
+        cout << endl << endl;
     }
-    else cout << "Error! File does not exist.";*/
+
+    for (int i = 0; i < vertex_count; i++) {
+        for (int j = 0; j < vertex_count; j++) {
+            if (matrix[i][j] == INF) cout << "There is no path from " << i << " to " << j << "." << endl << endl;
+            else {
+                cout << "The shortest path from " << i << " to " << j << ": [";
+                for (int k : get_path(from, i, j)) {
+                    cout << " " << k;
+                }
+                cout << " ]" << endl << endl;;
+            }
+        }
+    }
 }
