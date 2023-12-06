d30e6226fa48922853c472bcca680ad97f3784b8
Karakota1 <140350621+Karakota1@users.noreply.github.com>
2023-12-06 22:03:02 +0300

    Update Kondarev.cpp

--- DZ_Alg/Kondarev.cpp
+++ DZ_Alg/Kondarev.cpp
@@ -12,9 +12,10 @@ const int INF = 9999;
 
 void Kondarev(ifstream& fin) {
 
-    int n, ways_cnt;
+    auto start = chrono::system_clock::now();
+
     int n = tryChoose(fin, 0, 9999);
-    int ways_cnt = tryChoose(fin, 0, n*n);
+    int ways_cnt = tryChoose(fin, 0, 9999);
 
     vector<vector<int>> graph(n, vector<int>(n));
 
@@ -28,9 +29,9 @@ void Kondarev(ifstream& fin) {
 
     for (int i = 0; i < ways_cnt; i++)
     {
-        int index1 = tryChoose(fin);
-        int index2 = tryChoose(fin);
-        graph[index1 - 1][index2 - 1] = tryChoose(fin);
+        int index_1 = tryChoose(fin, 0, n - 1);
+        int index_2 = tryChoose(fin, 0, n - 1);
+        graph[index_1][index_2] = tryChoose(fin, -9999, 9999);;
     }
 
     vector<vector<int>> dist(n, vector<int>(n));
@@ -50,13 +51,17 @@ void Kondarev(ifstream& fin) {
         }
     }
 
+    auto finish = chrono::system_clock::now();
+    auto time = chrono::duration_cast<chrono::microseconds>(finish - start).count();
+    cout << "Time for method: " << time << " microseconds" << endl;
+
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             if (dist[i][j] == INF) {
-                cout << "INF   ";
+                cout << "      INF";
             }
             else {
-                cout << dist[i][j] << "   ";
+                cout << format("{:9}", dist[i][j]);
             }
         }
         cout << endl;
