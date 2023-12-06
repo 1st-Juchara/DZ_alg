e2bb09f6001fa91c862b45515e7018180ecfbfd6
Ernestufa <ernesttimirbaev@yandex.ru>
2023-12-07 02:47:36 +0300

    Djonson algorithm (Timirbaev Ernest)

--- DZ_Alg/TimirbaevErnest.cpp
+++ DZ_Alg/TimirbaevErnest.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <chrono>
+#include <set>
+#include <iterator>
 
 using namespace std;
 
@@ -12,17 +15,15 @@ struct Edge {
     int start, end, weight;
 };
 
-vector<int> BF(vector<Edge> const& edges, vector<int>& parent, int source, int n)
-{
 
-    vector<int> distance(n, INT_MAX);
-    distance[source] = 0;
+
+bool bellmanFord(vector<Edge>& edges, vector<int>& distance, int n) {
 
     int u, v, w, k = n;
 
     while (--k)
     {
-        for (Edge edge : edges)
+        for (Edge& edge : edges)
         {
 
             u = edge.start;
@@ -34,12 +35,11 @@ vector<int> BF(vector<Edge> const& edges, vector<int>& parent, int source, int n
 
                 distance[v] = distance[u] + w;
 
-                parent[v] = u;
             }
         }
     }
 
-    for (Edge edge : edges)
+    for (Edge& edge : edges)
     {
 
         u = edge.start;
@@ -49,11 +49,11 @@ vector<int> BF(vector<Edge> const& edges, vector<int>& parent, int source, int n
         if (distance[u] != INT_MAX && distance[u] + w < distance[v])
         {
             cout << "Negative-weight cycle is found!!";
-            return distance;
+            return false;
         }
     }
 
-    return distance;
+    return true;
 }
 
 
@@ -82,64 +82,81 @@ vector<int> dijkstra(vector<vector<pair<int, int>>>& graph, int start) {
 }
 
 
+void algorithm(vector<Edge>& edges, const int& vertex_count, const int& edge_count, const int& new_vertex) {
+    vector<int> distance(new_vertex + 1, INT_MAX);
+    distance[new_vertex] = 0;
+
+    if (bellmanFord(edges, distance, new_vertex + 1)) {
+
+        for (int i = 0; i < vertex_count; i++)
+            edges.erase(edges.begin() + 0);
+
+        for (auto& i : edges) {
+            i.weight = i.weight + distance[i.start] - distance[i.end];
+        }
+
+        vector<vector<pair<int, int>>> graph(new_vertex);
+        for (int i = 0; i < edge_count; i++) {
+            graph[edges[i].start].push_back({ edges[i].end, edges[i].weight });
+        }
+
+        for (int u = 0; u < new_vertex; u++) {
+            vector<int> dist = dijkstra(graph, u);
+
+            for (int v = 0; v < dist.size(); v++) {
+                if (dist[v] != INF)
+                    cout << dist[v] - distance[u] + distance[v] << '\t';
+                else
+                    cout << "X" << '\t';
+            }
+            cout << endl;
+        }
+
+    }
+}
+
+
 void TimirbaevErnest(std::ifstream& fin) {
+    ifstream file;
+    file.open("input.txt", ios::in);
 
     int vertex_count, edge_count;
+    set<int> vertex;
     vector<Edge> edges;
 
 
-    if (fin.is_open()) {
+    if (file.is_open()) {
 
-        vertex_count = tryChoose(fin, 0, 9999);
-        edge_count = tryChoose(fin, 0, vertex_count*vertex_count);
+        vertex_count = tryChoose(file, 0, 9999); edge_count = tryChoose(file, 0, vertex_count * vertex_count);
 
         int length = edge_count + vertex_count;
         edges.resize(length);
 
-        for (int i = 0; i < vertex_count; i++) {
-            edges[i].start = 0;
-            edges[i].end = i + 1;
-            edges[i].weight = 0;
-        }
+        while (!file.eof()) {
 
-        vertex_count += 1;
-
-        while (!fin.eof()) {
-
-            for (int i = vertex_count - 1; i < length; i++) {
-                edges[i].start = tryChoose(fin); edges[i].end = tryChoose(fin); edges[i].weight = tryChoose(fin);
+            for (int i = vertex_count; i < length; i++) {
+                edges[i].start = tryChoose(file); edges[i].end = tryChoose(file); edges[i].weight = tryChoose(file);
+                vertex.insert(edges[i].start);
+                vertex.insert(edges[i].end);
             }
         }
+
+        set<int>::iterator it_end = vertex.end();
+        set<int>::iterator it_begin = vertex.begin();
+        it_end--;
+        int new_vertex = *it_end + 1;
+
+        for (int i = 0; i < vertex_count; i++) {
+            edges[i].start = new_vertex;
+            edges[i].end = *it_begin;
+            edges[i].weight = 0;
+            it_begin++;
+        }
+
+        algorithm(edges, vertex_count, edge_count, new_vertex);
 
     }
 
     else cout << "Error! File does not exist.";
-
-    vector<int> parent(vertex_count, -1);
-    vector<int> distance = BF(edges, parent, 0, vertex_count);
-
-    for (int i = 0; i < vertex_count - 1; i++)
-        edges.erase(edges.begin() + 0);
-
-    for (auto& i : edges) {
-        i.weight = i.weight + distance[i.start] - distance[i.end];
-    }
-
-    vector<vector<pair<int, int>>> graph(vertex_count - 1);
-    for (int i = 0; i < edge_count; i++) {
-        graph[edges[i].start - 1].push_back({ edges[i].end - 1, edges[i].weight });
-    }
-
-    for (int u = 0; u < vertex_count - 1; u++) {
-        vector<int> dist = dijkstra(graph, u);
-
-        for (int v = 0; v < dist.size(); v++) {
-            if (dist[v] != INF)
-                cout << dist[v] - distance[u + 1] + distance[v + 1] << '\t';
-            else
-                cout << "X" << '\t';
-        }
-        cout << endl;
-    }
 
 }
